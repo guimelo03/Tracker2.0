205337f1952a6047434f17b5d25e116a43851d0b
guimelo03 <guilhermemelo03@hotmail.com>
2025-05-09 12:27:21 -0300

    primeiro commit!

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro' : modoEscuro}">
+    <div class="column is-one-quarter">
+      <BarraLateral @aoAlterarModo="alterarModo"/>
+    </div>
+    <div class="column is-three-quarters conteudo">
+      <router-view></router-view>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    BarraLateral,
+  },
+  data () {
+    return {
+      modoEscuro: false
+    }
+  },
+  methods: {
+    alterarModo (modoEscuro: boolean) : void {
+      this.modoEscuro = modoEscuro
+    }
+  }
+});
+</script>
+
+<style>
+html, body, #app, main {
+  height: 100vh;
+  margin: 0;
+}
+main {
+  --bg-primario: #fff;
+  --texto-primario: #000;
+  --borda-input: #ccc;
+  --bg-botao: #3273dc;       /* azul padrão Bulma */
+  --cor-botao: #fff;
+  --bg-tabela: #fff;
+  --cor-tabela: #000;
+}
+main.modo-escuro {
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+  --borda-input: #555;
+  --bg-botao: #4a4e69;
+  --cor-botao: #fff;
+  --bg-tabela: #1a1a1a;
+  --cor-tabela: #ddd;
+}
+.lista {
+  padding: 1.25rem;
+}
+.conteudo {
+  background-color: var(--bg-primario);
+}
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,88 @@
+<template>
+  <header>
+    <div class="logo-container">
+      <h1>
+        <img alt="Alura Tracker" src="../assets/logo.png">
+      </h1>
+      <div class="has-text-centered">
+        <button class="button" @click="alterarModo">Ativar modo {{ textoBtn }}</button>
+      </div>
+      <nav class="panel mt-3">
+        <ul>
+          <li>
+            <router-link to="/" class="link">
+              <i class="fas fa-tasks"></i>
+              Tarefas
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/projetos" class="link">
+              <i class="fas fa-project-diagram"></i>
+              Projetos
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "BarraLateral",
+  emits: ['aoAlterarModo'],
+  data () {
+    return {
+      modoEscuro: false
+    }
+  },
+  methods: {
+    alterarModo () : void {
+      this.modoEscuro = !this.modoEscuro
+      this.$emit('aoAlterarModo', this.modoEscuro)
+    }
+  },
+  computed: {
+    textoBtn () : string {
+      return this.modoEscuro ? 'claro' : 'escuro'
+    }
+  }
+});
+</script>
+<style scoped>
+h1 {
+  text-align: center;
+}
+strong {
+  color: #f95738;
+}
+header {
+  background: #0d3b66;
+  color: white;
+  height: 100%;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 2px solid red; /* para depurar */
+}
+@media only screen and (max-width: 768px) {
+  header {
+    height: auto;
+  }
+}
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #FAF0CA;
+}
+.link.router-link-active {
+  color: #FAF0CA;
+}
+</style>
